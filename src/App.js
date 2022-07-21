300c300e7dbad021aa17d24c467b98b6b92233fa
joy-ike <joy.ike@mongodb.com>
2022-07-21 02:22:30 +0200

    Added generic login form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import logo from './logo.svg';
+import React from "react";
+import ReactDOM from 'react-dom';
+import { LoginForm } from "./LoginForm";
+
+// Bootstrap
+//import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Tabs from "react-bootstrap/Tabs";
+import Tab from "react-bootstrap/Tab";
+import { Row, Col } from "react-bootstrap";
+
+// Custom CSS
 import './App.css';
 
-function App() {
+import * as Realm from "realm-web";
+// Add your App ID
+const app = new Realm.App({id: "myiot-sxwcb"});
+
+// Gets a valid Realm user access token to authenticate requests
+async function getValidAccessToken() {
+  // Guarantee that there's a logged in user with a valid access token
+  if (!app.currentUser) {
+    console.log("Error: No CurrentUser!");
+  } else {
+    // An already logged in user's access token might be stale. To guarantee that the token is
+    // valid, we refresh the user's custom data which also refreshes their access token.
+    await app.currentUser.refreshCustomData();
+  }
+  return app.currentUser.accessToken;
+}
+
+function App(props) {
+  // Keep the logged in Realm user in local state. This lets the app re-render
+  // whenever the current user changes (e.g. logs in or logs out).
+  const [user, setUser] = React.useState(app.currentUser);
+
+  // Conditional Rendering ->> https://reactjs.org/docs/conditional-rendering.html
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1 id="headerTitle">Login</h1>
+        <LoginForm app={app} setUser={setUser} />
+        
+    
     </div>
   );
 }
